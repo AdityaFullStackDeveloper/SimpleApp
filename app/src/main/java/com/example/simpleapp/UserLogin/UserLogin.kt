e8072c0c1e40a5faeeaa635119ee414982ee6ecc
aditya-kumar123 <150265694+AdityaFullStackDeveloper@users.noreply.github.com>
2024-03-11 14:31:14 +0530

    AppSimaple
    MyAppSimple

---
+++ app/src/main/java/com/example/simpleapp/UserLogin/UserLogin.kt
@@ -0,0 +1,15 @@
+package com.example.simpleapp.UserLogin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.simpleapp.R
+
+class UserLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_login)
+    }
+}
